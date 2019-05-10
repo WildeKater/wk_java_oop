5e6d3dbe6782560b7cb460c913c7a4ec4077ce8f
Alexey Antonenko <wildekatertz@gmail.com>
2019-05-10 12:36:25 +0300

    2.11

--- JOOP.L9.4/src/com/gmail/wildekatertz/Main.java
+++ JOOP.L9.4/src/com/gmail/wildekatertz/Main.java
@@ -20,15 +20,20 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int drink = sc.nextInt();
 
+		queue = calcQueue(queue, drink);
+
+		System.out.println(queue);
+
+	}
+
+	private static List<String> calcQueue(List<String> queue, int drink) {
 		for (int i = 0; i < drink; i++) {
 			String tmp = queue.get(0);
 			queue.remove(0);
 			queue.add(queue.size(), tmp);
 			queue.add(queue.size(), tmp);
 		}
-
-		System.out.println(queue);
-
+		return queue;
 	}
 
 }
